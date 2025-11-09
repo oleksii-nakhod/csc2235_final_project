caa78d0bff034172bc1e7d4b814ef0ddcc5eb5db
Oleksii Nakhod <26633244+oleksii-nakhod@users.noreply.github.com>
2025-11-09 15:18:46 -0500

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-# Test if script is working
 echo "Setup script is running..." >> /local/repository/setup.log
 
-# Wait for any automatic system updates to finish
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3-venv python3-pip
 
-# Install pip
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip
+# Create and activate a virtual environment
+python3 -m venv /local/repository/venv
+source /local/repository/venv/bin/activate
 
-# Install your project's dependencies
-# As per section 4.4, the repo is cloned to /local/repository
-pip3 install -r /local/repository/requirements.txt
+# Install dependencies inside the venv
+pip install --upgrade pip
+pip install -r /local/repository/requirements.txt
